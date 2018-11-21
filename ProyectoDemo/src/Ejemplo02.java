7b5e0cf133d83e3e5c38f479514d22378837597e
María Falcón Elías <malufael@hotmail.com>
2018-11-20 21:44:18 -0500

    segundo proyecto

---
+++ ProyectoDemo/src/Ejemplo02.java
@@ -0,0 +1,27 @@
+
+public class Ejemplo02 {
+    int codigo;
+    String nombre;
+
+    public Ejemplo02(int codigo, String nombre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+}
